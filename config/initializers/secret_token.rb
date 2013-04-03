990f2d67700dd20cc383314dde872445e071a342
Deep Marreddy & Michael Kaiser-Nyman <student@epicodus.com>
2013-04-03 09:44:07 -0700

    basic rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coachatlas::Application.config.secret_token = 'f3b3845b54beac55f8e7050d926b716c892e8dc39a16971c29bac653ee88208b73c33a29fe575975258dd2a605381cd3eea6b956d2437ff49d21323e16efe511'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Coachatlas::Application.config.session_store :cookie_store, key: '_coachatlas_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Coachatlas::Application.config.session_store :active_record_store
